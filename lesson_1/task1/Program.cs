fe2e19681f10174b3994170c03876e815cd90558
Sasha Stahov <flex.black@yandex.ru>
2024-02-16 17:21:21 +0300

    done 1st task1

--- lesson_1/task1/Program.cs
+++ lesson_1/task1/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Просто проверяю как оно работает")
+﻿using System.Data;
+
+Console.WriteLine("Введите число а");
+string num1 = Console.ReadLine();
+int a = int.Parse(num1);
+Console.WriteLine("Введите число b");
+string num2 = Console.ReadLine();
+int b = int.Parse(num2);
+if (a == b*b)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
